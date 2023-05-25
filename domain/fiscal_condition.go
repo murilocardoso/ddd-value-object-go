f6729a9eb98395bfc88f2b65ab1757c0671a66c0
Murilo Carlos Cardoso <murilo.cardoso@mercadolivre.com>
2023-05-25 00:53:10 -0300

    feat: lidando com valores desnormalizados

--- domain/fiscal_condition.go
+++ domain/fiscal_condition.go
@@ -4,6 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"github.com/thoas/go-funk"
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+	"strings"
+	"unicode"
 )
 
 type FiscalCondition string
@@ -15,7 +20,15 @@ const (
 )
 
 func NewFiscalConditionFromString(aFiscalCondition string) (FiscalCondition, error) {
+	normalizedFiscalCondition, err := normalizeFiscalCondition(aFiscalCondition)
+	if err != nil {
+		return "", err
+	}
 
+	return newFiscalCondition(normalizedFiscalCondition)
+}
+
+func newFiscalCondition(aFiscalCondition string) (FiscalCondition, error) {
 	fiscalCondition := FiscalCondition(aFiscalCondition)
 
 	if !fiscalCondition.valid() {
@@ -29,4 +42,18 @@ func (f *FiscalCondition) valid() bool {
 	validValues := []string{string(ConsumidorFinal), string(Monotributo), string(ResponsableInscripto)}
 	// go get github.com/thoas/go-funk
 	return funk.Contains(validValues, string(*f))
+}
+
+func normalizeFiscalCondition(aFiscalCondition string) (string, error) {
+	// go get golang.org/x/text/transform | golang.org/x/text/unicode/norm | golang.org/x/text/runes
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	normalizedFiscalCondition, _, err := transform.String(t, aFiscalCondition)
+	if err != nil {
+		return "", err
+	}
+
+	normalizedFiscalCondition = strings.ReplaceAll(normalizedFiscalCondition, " ", "_")
+	normalizedFiscalCondition = strings.ToUpper(normalizedFiscalCondition)
+
+	return normalizedFiscalCondition, err
 }
--- main/entrypoint.go
+++ main/entrypoint.go
@@ -9,12 +9,17 @@ import (
 /*
 Uma possível forma de resolver é convencionar usar uma função que encapsule as validações dos valores válidos, nesse
 caso seria uma função construtora do tipo FiscalCondition
+
+Aqui há uma oportunidade de encapsular este regra de normalização, pode ser que alguns casos de uso venham
+normalizado e em outros não, por não ser uma operação cara, pode-se encapsular e sempre normalizar antes de criar.
+
+Pronto, agora conseguimos lidar também com valores desnormalizados e está encapsulado!
 */
 
 func main() {
 	var fiscalCondition domain.FiscalCondition
 
-	fiscalCondition, err := domain.NewFiscalConditionFromString("MONOTRIBUTO")
+	fiscalCondition, err := domain.NewFiscalConditionFromString("Monotributo")
 
 	fmt.Println(strings.Repeat("-", 30))
 	fmt.Println(fmt.Sprintf("FISCAL_CONDITION: %s", fiscalCondition))
