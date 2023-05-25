487cb05e7ed0ef7b44afab6f3dca6e5ebbfe58b2
Murilo Carlos Cardoso <murilo.cardoso@mercadolivre.com>
2023-05-25 00:16:32 -0300

    refactor: fiscal condition as alias type to string

---
+++ domain/fiscal_condition.go
@@ -0,0 +1,9 @@
+package domain
+
+type FiscalCondition string
+
+const (
+	ConsumidorFinal      FiscalCondition = "CONSUMIDOR_FINAL"
+	Monotributo          FiscalCondition = "MONOTRIBUTO"
+	ResponsableInscripto FiscalCondition = "RESPONSABLE_INSCRIPTO"
+)
--- main/entrypoint.go
+++ main/entrypoint.go
@@ -2,45 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/murilocardoso/ddd-value-object-go/domain"
 	"strings"
 )
 
 /*
-Olhando o exemplo de uma string solta parece um pouco forçado, mas usando dentro de uma estrutura eu diria que é bem
-comum encontrar.
+Uma evolução para o nosso modelo seria criar um tipo FiscalCondition e definir constantes para os possíveis valores,
+olhando muitos códigos go, essa abordagem é bastante usada
 */
-type Taxpayer struct {
-	FiscalCondition string
-}
 
 func main() {
+	var fiscalCondition domain.FiscalCondition
 
-	var taxpayer Taxpayer
-
-	taxpayer.FiscalCondition = "Monotributo"
+	fiscalCondition = domain.Monotributo
 
 	fmt.Println(strings.Repeat("-", 30))
-	fmt.Println(taxpayer.FiscalCondition)
+	fmt.Println(fiscalCondition)
 	fmt.Println(strings.Repeat("-", 30))
 }
-
-/*
-Modelando como uma simples string...
-*/
-
-/*
-O problema dessa abordagem é que praticamente não existe tipagem, assim como coloquei Monotributo
-poderia ter colocado Murilo. Ou seja, é um modelo que diz muito pouco a respeito do que é uma condição fiscal e
-mais importante, não garante consistência
-*/
-
-/*
-Aqui vale mencionar um ponto, creio que a principal DIFERENÇA ENTRE CLEAN ARCH E DDD é que clean arch, como o próprio
-nome já diz tem relação com a arquitetura e organização das camadas, foca bastante nos casos de uso. Não que casos de
-uso não sejam importantes, eles definitivamente são, mas O CASO DE USO TEM QUE FAZER USO DOS MODELOS DE DOMÍNIO QUE
-DEVEM GARANTIR A SUA PRÓPRIA CONSISTÊNCIA, caso contrário a manipulação das informações das entidades fica sobre a
-responsabilidade dos diversos possíveis casos de uso e desta forma o PRINCÍPIO DO ENCAPSULAMENTO SE PERDE.
-Para ENTENDER UMA CONDIÇÃO FISCAL TERIA QUE CONHECER O QUE TODOS CASOS DE USO fazem com a string fora o risco de
-inconsistência.
-
-*/
