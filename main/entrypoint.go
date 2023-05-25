ad1dda63d3c005ac6523f6569dd34f8c419892ad
Murilo Carlos Cardoso <murilo.cardoso@mercadolivre.com>
2023-05-25 00:45:11 -0300

    chore: provando valor valido para o novo construtor

--- main/entrypoint.go
+++ main/entrypoint.go
@@ -14,7 +14,7 @@ caso seria uma função construtora do tipo FiscalCondition
 func main() {
 	var fiscalCondition domain.FiscalCondition
 
-	fiscalCondition, err := domain.NewFiscalConditionFromString("Monotributo")
+	fiscalCondition, err := domain.NewFiscalConditionFromString("MONOTRIBUTO")
 
 	fmt.Println(strings.Repeat("-", 30))
 	fmt.Println(fmt.Sprintf("FISCAL_CONDITION: %s", fiscalCondition))
